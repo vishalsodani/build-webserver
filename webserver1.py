bed134dadfde3a724fe30a8a0364869802c8a57e
vishalsodani <vishal_sodani@yahoo.com>
2019-09-01 10:19:39 +0000

    added some documentation

--- webserver1.py
+++ webserver1.py
@@ -1,3 +1,5 @@
+#https://docs.python.org/3/howto/sockets.html
+
 import socket
 
 HOST, PORT = '', 13119
@@ -11,9 +13,17 @@ listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #https://www.gnu.org/software/libc/manual/html_node/Socket_002dLevel-Options.html#Socket_002dLevel-Options
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
+# what is 1 in the argument?
+# the argument to listen tells the socket library that we want it to queue up as many as 1 connect requests 
+# before refusing outside connections
 listen_socket.listen(1)
 
 while True:
+    #http://man7.org/linux/man-pages/man2/accept.2.html
+    #extracts the first connection
+    #request on the queue of pending connections for the listening socket,
+    #sockfd, creates a new connected socket, and returns a new file
+    #descriptor referring to that socket
     client_connection, client_address = listen_socket.accept()
     request_data = client_connection.recv(1024)
     print(request_data.decode('utf-8'))
